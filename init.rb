256158833ecf1fc95095c66f23662d68c887aa71
ande3577 <ande3577@gmail.com>
2015-02-06 18:37:03 -0600

    Initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_filter_issues_by_parent_task do
+  name 'Redmine Filter Issues By Parent Task plugin'
+  author 'David S Anderson'
+  description 'Add parent task to list of available issue filters'
+  version '0.0.1'
+  url 'https://github.com/ande3577/redmine_filter_issues_by_parent_task'
+  author_url 'https://github.com/ande3577/'
+end
